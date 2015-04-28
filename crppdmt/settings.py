d4fcec2f625d6ba76a015376f174fce11acb8af0
miquel <miquel.corral@gmail.com>
2015-04-28 22:06:18 +0200

    problem with staticfiles

--- crppdmt/settings.py
+++ crppdmt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'urk$y)-v%z=65jf@e@7_zg*h=ne)s3c^&14plbw(#hrctdpxnm'
 if "LOCAL" == deploy_env:
     DEBUG = True
 else:
-    DEBUG = False
+    DEBUG = True
 
 
 TEMPLATE_DEBUG = True
@@ -91,7 +91,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../crppdmt/static'),
+    os.path.join(BASE_DIR, '../crppdmt/static/'),
 )
 
 # date input formats
