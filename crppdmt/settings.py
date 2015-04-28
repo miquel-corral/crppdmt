e1b5179022d8364cef48a0a6d62ae55b48e19d95
miquel <miquel.corral@gmail.com>
2015-04-28 22:11:08 +0200

    problem with staticfiles

--- crppdmt/settings.py
+++ crppdmt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'urk$y)-v%z=65jf@e@7_zg*h=ne)s3c^&14plbw(#hrctdpxnm'
 if "LOCAL" == deploy_env:
     DEBUG = True
 else:
-    DEBUG = True
+    DEBUG = False
 
 
 TEMPLATE_DEBUG = True
